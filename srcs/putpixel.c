f2b5e846bb55c16e5ed9e2d5a10a61f72656eacd
Tiago LERNOULD <tiagolernould@hotmail.com>
2018-01-16 19:05:45 +0100

    tweaks

--- srcs/putpixel.c
+++ srcs/putpixel.c
@@ -6,7 +6,7 @@
 /*   By: tlernoul <tlernoul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/16 18:07:37 by tlernoul          #+#    #+#             */
-/*   Updated: 2018/01/16 18:16:42 by tlernoul         ###   ########.fr       */
+/*   Updated: 2018/01/16 18:48:33 by tlernoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,17 @@ void	putpixel(t_pnt pt, float hue)
 	hsl.s = 1.0;
 	hsl.l = 0.5;
 	get_env()->img.data[pt.y * W_WDTH + pt.x] = ft_hsl_to_rgb(hsl) & 0x00FFFFFF;
+}
+
+void	putpixel_opt(t_pnt, float hue)
+{
+	t_hsl hsl;
+
+	hsl.h = hue;
+	hsl.s = 1.0;
+	hsl.l = 0.5;
+	SDL_SetSurfaceRLE(env->surf, 1);
+	SDL_LockSurface(env->surf);
+	get_env()->img.data[pt.y * W_WDTH + pt.x] = ft_hsl_to_rgb(hsl) & 0x00FFFFFF;
+	SDL_UnlockSurface(env->surf);
 }
