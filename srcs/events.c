de48600ae037230f98d84adb5c052873b53a15d9
Theo LE GAC--DUFLY <tle-gac-@e3r13p10.42.fr>
2018-01-23 21:04:05 +0100

    Can move forward, backward and strafe.

--- srcs/events.c
+++ srcs/events.c
@@ -6,26 +6,63 @@
 /*   By: tlernoul <tlernoul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/22 14:03:53 by tlernoul          #+#    #+#             */
-/*   Updated: 2018/01/23 14:56:44 by tlernoul         ###   ########.fr       */
+/*   Updated: 2018/01/23 20:57:53 by tle-gac-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/wolf.h"
 
+void	move_dir(int sense, t_env *env)
+{
+	float	x;
+	float	y;
+	float	mag;
+
+	mag = sqrt(env->dir_vec.x * env->dir_vec.x + env->dir_vec.y * env->dir_vec.y);
+	x = 0.05 * env->dir_vec.x / mag * (env->dir_vec.x == 0 ? 0 : env->dir_vec.x / fabs(env->dir_vec.x)) * sense;
+	y = 0.05 * env->dir_vec.y / mag * (env->dir_vec.y == 0 ? 0 : env->dir_vec.y / fabs(env->dir_vec.y)) * sense;
+	printf("New pos : %f %f\n", env->ppos.x + x, env->ppos.y + y);
+	if(env->map.map[(int)(env->ppos.x + x)][(int)(env->ppos.y + y)] != '#')
+	{
+		env->ppos.x += x;
+		env->ppos.y += y;
+	}
+}
+
+void	strafe(int sense, t_env *env)
+{
+	float	x;
+	float	y;
+	float	mag;
+
+	mag = sqrt(env->cam_vec.x * env->cam_vec.x + env->cam_vec.y * env->cam_vec.y);
+	x = 0.05 * env->cam_vec.x / mag * (env->cam_vec.x == 0 ? 0 : env->cam_vec.x / fabs(env->cam_vec.x)) * sense;
+	y = 0.05 * env->cam_vec.y / mag * (env->cam_vec.y == 0 ? 0 : env->cam_vec.y / fabs(env->cam_vec.y)) * sense;
+	printf("New pos : %f %f\n", env->ppos.x + x, env->ppos.y + y);
+	if(env->map.map[(int)(env->ppos.x + x)][(int)(env->ppos.y + y)] != '#')
+	{
+		env->ppos.x += x;
+		env->ppos.y += y;
+	}
+	
+}
+
 void	movements(t_env *env)
 {
 	if (env->state[SDL_SCANCODE_UP])
-		printf("up !\n");
+		move_dir(-1, env);
 	if (env->state[SDL_SCANCODE_DOWN])
-		printf("down !\n");
+		move_dir(1, env);
 	if (env->state[SDL_SCANCODE_X] && env->state[SDL_SCANCODE_LEFT])
-		printf("strafe left !\n");
+		strafe(-1, env);
 	else if (env->state[SDL_SCANCODE_LEFT])
 		printf("left !\n");
 	if (env->state[SDL_SCANCODE_X] && env->state[SDL_SCANCODE_RIGHT])
-		printf("strafe right !\n");
+		strafe(1, env);
 	else if (env->state[SDL_SCANCODE_RIGHT])
 		printf("right !\n");
+	raycasting(env);
+	SDL_UpdateWindowSurface(env->win_p);
 	//redraw
 }
 
