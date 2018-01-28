f750f5130e5a965d49b69041eb8ee5b1bc2d4f8e
Theo LE GAC--DUFLY <tle-gac-@e2r8p8.42.fr>
2018-01-28 21:11:54 +0100

    Last norming in events.c Now everything should be hunky-doory.

--- srcs/events.c
+++ srcs/events.c
@@ -6,7 +6,7 @@
 /*   By: tlernoul <tlernoul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/22 14:03:53 by tlernoul          #+#    #+#             */
-/*   Updated: 2018/01/28 21:04:53 by tlernoul         ###   ########.fr       */
+/*   Updated: 2018/01/28 21:11:04 by tle-gac-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,12 @@ void	move_dir(int sense, t_env *env)
 	float	y;
 	float	mag;
 
-	mag = sqrt(env->dir_vec[0] * env->dir_vec[0] + env->dir_vec[1] * env->dir_vec[1]);
-	x = 0.05 * env->dir_vec[0] / mag * (env->dir_vec[0] == 0 ? 0 : env->dir_vec[0] / fabs(env->dir_vec[0])) * (env->dir_vec[0] < 0 ? -1 : 1) * sense;
-	y = 0.05 * env->dir_vec[1] / mag * (env->dir_vec[1] == 0 ? 0 : env->dir_vec[1] / fabs(env->dir_vec[1])) * (env->dir_vec[1] < 0 ? -1 : 1) * sense;
+	mag = sqrt(env->dir_vec[0] * env->dir_vec[0] + env->dir_vec[1] * \
+															env->dir_vec[1]);
+	x = 0.05 * env->dir_vec[0] / mag * (env->dir_vec[0] == 0 ? 0 : env->dir_vec\
+		[0] / fabs(env->dir_vec[0])) * (env->dir_vec[0] < 0 ? -1 : 1) * sense;
+	y = 0.05 * env->dir_vec[1] / mag * (env->dir_vec[1] == 0 ? 0 : env->dir_vec\
+		[1] / fabs(env->dir_vec[1])) * (env->dir_vec[1] < 0 ? -1 : 1) * sense;
 	if (env->map.map[(int)(env->ppos[0] + x)][(int)(env->ppos[1] + y)] != '#')
 	{
 		env->ppos[0] += x;
@@ -34,10 +37,13 @@ void	strafe(int sense, t_env *env)
 	float	y;
 	float	mag;
 
-	mag = sqrt(env->cam_vec[0] * env->cam_vec[0] + env->cam_vec[1] * env->cam_vec[1]);
-	x = 0.05 * env->cam_vec[0] / mag * (env->cam_vec[0] == 0 ? 0 : env->cam_vec[0] / fabs(env->cam_vec[0])) * (env->cam_vec[0] < 0 ? -1 : 1) * sense;
-	y = 0.05 * env->cam_vec[1] / mag * (env->cam_vec[1] == 0 ? 0 : env->cam_vec[1] / fabs(env->cam_vec[1])) * (env->cam_vec[1] < 0 ? -1 : 1) * sense;
-	if(env->map.map[(int)(env->ppos[0] + x)][(int)(env->ppos[1] + y)] != '#')
+	mag = sqrt(env->cam_vec[0] * env->cam_vec[0] + env->cam_vec[1] * \
+															env->cam_vec[1]);
+	x = 0.05 * env->cam_vec[0] / mag * (env->cam_vec[0] == 0 ? 0 : env->cam_vec\
+		[0] / fabs(env->cam_vec[0])) * (env->cam_vec[0] < 0 ? -1 : 1) * sense;
+	y = 0.05 * env->cam_vec[1] / mag * (env->cam_vec[1] == 0 ? 0 : env->cam_vec\
+		[1] / fabs(env->cam_vec[1])) * (env->cam_vec[1] < 0 ? -1 : 1) * sense;
+	if (env->map.map[(int)(env->ppos[0] + x)][(int)(env->ppos[1] + y)] != '#')
 	{
 		env->ppos[0] += x;
 		env->ppos[1] += y;
@@ -49,10 +55,12 @@ void	rotate(int sense, t_env *env)
 	float	tmp;
 
 	tmp = env->dir_vec[0] * cos(0.04) - env->dir_vec[1] * sin(0.04) * sense;
-	env->dir_vec[1] = env->dir_vec[0] * sin(0.04) * sense + env->dir_vec[1] * cos(0.04);
+	env->dir_vec[1] = env->dir_vec[0] * sin(0.04) * sense + env->dir_vec[1] * \
+																	cos(0.04);
 	env->dir_vec[0] = tmp;
 	tmp = env->cam_vec[0] * cos(0.04) - env->cam_vec[1] * sin(0.04) * sense;
-	env->cam_vec[1] = env->cam_vec[0] * sin(0.04) * sense + env->cam_vec[1] * cos(0.04);
+	env->cam_vec[1] = env->cam_vec[0] * sin(0.04) * sense + env->cam_vec[1] * \
+																	cos(0.04);
 	env->cam_vec[0] = tmp;
 }
 
